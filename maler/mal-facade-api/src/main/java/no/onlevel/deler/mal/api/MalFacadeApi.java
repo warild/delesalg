ad4e3ae842d12f6aa1ace939f0874078069cf181
Arild Spikkeland <aril-spi@online.no>
2014-09-27 11:52:43 +0200

    Endret prosjektoppbygging

---
+++ maler/mal-facade-api/src/main/java/no/onlevel/deler/mal/api/MalFacadeApi.java
@@ -0,0 +1,26 @@
+package no.onlevel.deler.mal.api;
+
+import java.util.List;
+
+import no.onlevel.deler.mal.domain.Egenskap;
+import no.onlevel.deler.mal.domain.Gruppe;
+/**
+ * Metoder for å vise, opprette, endre og slette maler
+ *
+ */
+public interface MalFacadeApi {
+
+	List<Gruppe> hentMaler();
+	
+	Gruppe hentMal(int malId);
+	
+	void opprettMal(Gruppe mal, List<Egenskap> egenskapListe);
+	
+	void endreMal(int malId, Gruppe mal);
+
+	void leggTilEgenskap(int malId, Egenskap egenskap);
+	
+	void slettEgenskap(int malId, int egenskapId);
+	
+	void slettMal(String malNavn, List<Egenskap> egenskapListe);
+}
