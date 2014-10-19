d541e46d3bed37072b6b92fcd8e0e8e793a04a6c
Arild Spikkeland <aril-spi@online.no>
2014-10-19 23:11:17 +0200

    POM oppdatering. Gui start

--- maler/mal-gui-fx/src/main/java/no/delesalg/maler/gui/MalRedigeringApplikasjon.java
+++ maler/mal-gui-fx/src/main/java/no/delesalg/maler/gui/MalRedigeringApplikasjon.java
@@ -5,35 +5,35 @@ import java.util.List;
 
 import javafx.application.Application;
 import javafx.beans.property.SimpleStringProperty;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.control.TreeItem;
 import javafx.scene.control.TreeView;
-import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
-import javafx.scene.layout.Pane;
-import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
-import javafx.scene.shape.Line;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
-import no.delesalg.maler.gui.DelesalgApplikasjon.Employee;
-import no.onlevel.deler.mal.domain.Egenskap;
 /** 
  * Gui for håndtering av maler
  *
  */
-public class DelesalgApplikasjon extends Application {
+public class MalRedigeringApplikasjon extends Application {
 	public static void main(String[] args) {
-		launch(DelesalgApplikasjon.class, args);		
+		launch(MalRedigeringApplikasjon.class, args);		
 	}
 
 
     
-    private final Node rootIcon = 
-            new ImageView();
+    private final Node rootIcon = new ImageView();
 
         List<Employee> employees = Arrays.<Employee>asList(
                 new Employee("Ethan Williams", "Sales Department"),
@@ -51,24 +51,50 @@ public class DelesalgApplikasjon extends Application {
             new TreeItem<String>("MyCompany Human Resources", rootIcon);
 
         
+      /* 
+       * Stage = window
+       * Scene = container: inneholder scener eller controller som skal vises
+       * Node = controller = scene
+       * 
+  HBox og VBox - legger ut elementene horisontalt (x-dimensjonen, dvs. bortover) eller vertikalt (y-dimensjonen, dvs. nedover) og gjør dem like store i den andre dimensjonen.
+  TilePane   - plasserer elementene i et rutenett, TilePane gjør alle ruten like store 
+  GridPane    - plasserer elementene i et rutenett, GridPane er mer fleksibel.
+  BorderPane - fordeler elementene i regioner, midten (for hovedinnholdet), top, bunn, venstre og høyre, og passer for hovedinnholdet i applikasjonen.
+  AnchorPane - henger elementene fast i punkter knyttet til en eller flere av sidene
+  Pane - generell container uten spesifikk layout, så posisjonen på elementer må settes manuelt.
+ 
+       * 
+       * 
+       */
 	@Override
 	public void start(Stage stage) throws Exception {
-        //Stage = window
-        //scene = container: inneholder scener eller controller som skal vises
-        // node = controller = scene
-        
-//  HBox og VBox - legger ut elementene horisontalt (x-dimensjonen, dvs. bortover) eller vertikalt (y-dimensjonen, dvs. nedover) og gjør dem like store i den andre dimensjonen.
-//  TilePane   - plasserer elementene i et rutenett, TilePane gjør alle ruten like store 
-//  GridPane    - plasserer elementene i et rutenett, GridPane er mer fleksibel.
-//  BorderPane - fordeler elementene i regioner, midten (for hovedinnholdet), top, bunn, venstre og høyre, og passer for hovedinnholdet i applikasjonen.
-//  AnchorPane - henger elementene fast i punkter knyttet til en eller flere av sidene
-//  Pane - generell container uten spesifikk layout, så posisjonen på elementer må settes manuelt.
-        
+		 stage.setTitle("Varelager");
 
+		 GridPane grid = new GridPane();
+		 grid.setAlignment(Pos.CENTER);
+		 grid.setHgap(10);
+		 grid.setVgap(10);
+		 grid.setPadding(new Insets(25, 25, 25, 25));
+
+		 Scene scene = new Scene(grid, 300, 275);
+		 stage.setScene(scene);
+
+		 Text scenetitle = new Text("Welcome");
+		 scenetitle.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
+		 grid.add(scenetitle, 0, 0, 2, 1);
+
+		 Label userName = new Label("User Name:");
+		 grid.add(userName, 0, 1);
+
+		 TextField userTextField = new TextField();
+		 grid.add(userTextField, 1, 1);
+
+		 Label pw = new Label("Password:");
+		 grid.add(pw, 0, 2);
+
+		 PasswordField pwBox = new PasswordField();
+		 grid.add(pwBox, 1, 2);
 		
-        
-
-	     
 
 	            rootNode.setExpanded(true);
 	            for (Employee employee : employees) {
@@ -91,10 +117,10 @@ public class DelesalgApplikasjon extends Application {
 	                }
 	            }
 	     
-	            stage.setTitle("Tree View Sample");
+
 	            VBox box = new VBox();
-	            final Scene scene = new Scene(box, 400, 300);
-	            scene.setFill(Color.LIGHTGRAY);
+//	            final Scene scene = new Scene(box, 400, 300);
+//	            scene.setFill(Color.LIGHTGRAY);
 	     
 	            TreeView<String> treeView = new TreeView<String>(rootNode);
 	            
